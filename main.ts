49edde4e71b18a57334aa19b395f14b84cd8f9ce
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2021-09-24 10:07:44 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    playerSprite.change(LedSpriteProperty.X, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    playerSprite.change(LedSpriteProperty.X, 1)
+})
+let playerSprite: game.LedSprite = null
+playerSprite = game.createSprite(2, 4)
+basic.forever(function () {
+	
+})
